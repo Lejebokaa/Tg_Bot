e12d612b3bfc74300441270540ca59e33c2886ec
301 student <301 student@klg.top-academy.internal>
2024-05-15 22:00:10 +0200

    Good night

--- handlers/weather_by_city.py
+++ handlers/weather_by_city.py
@@ -5,8 +5,8 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message, ReplyKeyboardRemove
 from keyboards.common_keyboards import main_keyboard
 from states.common_states import UserStates
-import aiohttp
-
+# import aiohttp
+import requests
 
 router = Router()
 
@@ -22,5 +22,13 @@ async def get_city_name(message: Message, state: FSMContext):
 @router.message(F.text, UserStates.user_choose_city)
 async def get_city_whether(message: Message, state: FSMContext):
     city = message.text
-    api = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b8af433cff7af5d1542af7bcca7e2634&units=metric"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=b8af433cff7af5d1542af7bcca7e2634&units=metric"
+    req = requests.get(url).json()
+    await message.reply(
+        text=f"Описание: {req['weather'][0]['main']};\n"
+             f"Температура: {req['main']['temp']};\n")
 
+    # async with aiohttp.ClientSession as session:
+    #     async with session.get(url=url, ) as response:
+    #         weather_data = await response.text()
+    #         print(weather_data)
